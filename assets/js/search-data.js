034ef75621e1a300a06c5bee9db14d090dd82c26
gouki510 <77923603+gouki510@users.noreply.github.com>
2025-05-26 13:29:27 +0000

    Deploying to gh-pages from @ gouki510/gouki510.github.io@274b1b332449feeed1a9a90276dbb0e49de7be37 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,14 +459,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6D%69%6E%65%67%69%73%68%69@%77%65%62%6C%61%62.%74.%75-%74%6F%6B%79%6F.%61%63.%6A%70", "_blank");
         },
       },{
         id: 'social-rss',
@@ -480,14 +473,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=sxGpoYMAAAAJ&hl", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/GoukiMinegishi", "_blank");
         },
       },{
       id: 'light-theme',
