e7a208818eefeda09b5d8aa936e29eb8dd20b1eb
gouki510 <77923603+gouki510@users.noreply.github.com>
2025-09-13 08:33:34 +0000

    Deploying to gh-pages from @ gouki510/gouki510.github.io@96f9db8986dd3acbd5c38163f5603a6867ca0875 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-japanese",
-          title: "japanese",
+        },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
